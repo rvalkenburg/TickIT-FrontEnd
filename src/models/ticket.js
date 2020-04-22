c46beb73d0d645fbaa0b8e46d3ccd8bf4b98906a
rvalkenburg <rogervalkenburg@gmail.com>
2020-04-22 23:31:17 +0200

    Removed unused component

---
+++ src/models/ticket.js
@@ -0,0 +1,9 @@
+export default class Ticket {
+    constructor(company, user, agent, title, description) {
+      this.company = company;
+      this.user = user;
+      this.agent = agent;
+      this.title = title;
+      this.description = description;
+    }
+  }
